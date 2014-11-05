4721412ef7047e8fcdab07d18de2f306ffea1a63
Daniel X Moore <yahivin@gmail.com>
2014-11-05 12:18:14 -0800

    :taxi::wheelchair: Updated in browser at strd6.github.io/editor

--- docs/package.js
+++ docs/package.js
@@ -180,34 +180,34 @@
     },
     "main.coffee.md": {
       "path": "main.coffee.md",
-      "content": "SVG Test\n========\n\n    style = document.createElement(\"style\")\n    style.innerHTML = require \"./style\"\n    document.head.appendChild style\n\n    paper = new Raphael(document.body, \"100%\", \"100%\")\n    rect = paper.rect(20, 20, 20, 20).attr\n      fill: '#F00'\n\n    anim = Raphael.animation({transform: \"r360\"}, 1000).repeat(Infinity)\n    rect.animate(anim)\n",
+      "content": "SVG Test\n========\n\n    {width, height} = require \"./pixie\"\n\n    style = document.createElement(\"style\")\n    style.innerHTML = require \"./style\"\n    document.head.appendChild style\n\n    paper = new Raphael(document.body, \"100%\", \"100%\")\n    rect = paper.rect(0, 0, 50, 50).attr\n      fill: '#F00'\n\n    lines = 25\n    [0...3*lines].forEach (n) ->\n      m = width/lines\n      line = paper.path( [\"M\", n * m - width, height, \"L\", width/2, height/2 ] )\n\n    anim = Raphael.animation({transform: \"r360\"}, 10000).repeat(Infinity)\n    rect.animate(anim)\n",
       "mode": "100644"
     },
     "pixie.cson": {
       "path": "pixie.cson",
-      "content": "remoteDependencies: [\n  \"https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js\"\n]\n",
+      "content": "remoteDependencies: [\n  \"https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js\"\n]\nwidth: 960\nheight: 540\n",
       "mode": "100644"
     },
     "style.styl": {
       "path": "style.styl",
-      "content": "html\n  height: 100%\n\nbody\n  background-color: blue\n  margin: 0\n  height: 100%\n",
+      "content": "html\n  height: 100%\n\nbody\n  height: 100%\n  margin: 0\n  overflow: hidden\n",
       "mode": "100644"
     }
   },
   "distribution": {
     "main": {
       "path": "main",
-      "content": "(function() {\n  var anim, paper, rect, style;\n\n  style = document.createElement(\"style\");\n\n  style.innerHTML = require(\"./style\");\n\n  document.head.appendChild(style);\n\n  paper = new Raphael(document.body, \"100%\", \"100%\");\n\n  rect = paper.rect(20, 20, 20, 20).attr({\n    fill: '#F00'\n  });\n\n  anim = Raphael.animation({\n    transform: \"r360\"\n  }, 1000).repeat(Infinity);\n\n  rect.animate(anim);\n\n}).call(this);\n",
+      "content": "(function() {\n  var anim, height, lines, paper, rect, style, width, _i, _ref, _ref1, _results;\n\n  _ref = require(\"./pixie\"), width = _ref.width, height = _ref.height;\n\n  style = document.createElement(\"style\");\n\n  style.innerHTML = require(\"./style\");\n\n  document.head.appendChild(style);\n\n  paper = new Raphael(document.body, \"100%\", \"100%\");\n\n  rect = paper.rect(0, 0, 50, 50).attr({\n    fill: '#F00'\n  });\n\n  lines = 25;\n\n  (function() {\n    _results = [];\n    for (var _i = 0, _ref1 = 3 * lines; 0 <= _ref1 ? _i < _ref1 : _i > _ref1; 0 <= _ref1 ? _i++ : _i--){ _results.push(_i); }\n    return _results;\n  }).apply(this).forEach(function(n) {\n    var line, m;\n    m = width / lines;\n    return line = paper.path([\"M\", n * m - width, height, \"L\", width / 2, height / 2]);\n  });\n\n  anim = Raphael.animation({\n    transform: \"r360\"\n  }, 10000).repeat(Infinity);\n\n  rect.animate(anim);\n\n}).call(this);\n",
       "type": "blob"
     },
     "pixie": {
       "path": "pixie",
-      "content": "module.exports = {\"remoteDependencies\":[\"https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js\"]};",
+      "content": "module.exports = {\"remoteDependencies\":[\"https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js\"],\"width\":960,\"height\":540};",
       "type": "blob"
     },
     "style": {
       "path": "style",
-      "content": "module.exports = \"html {\\n  height: 100%;\\n}\\n\\nbody {\\n  background-color: blue;\\n  margin: 0;\\n  height: 100%;\\n}\";",
+      "content": "module.exports = \"html {\\n  height: 100%;\\n}\\n\\nbody {\\n  height: 100%;\\n  margin: 0;\\n  overflow: hidden;\\n}\";",
       "type": "blob"
     }
   },
